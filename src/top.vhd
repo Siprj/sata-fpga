2d3489d1c749449971d5a3668d11934a51d1c11c
Jan Sipr <sipr.jan@gmail.com>
2017-04-11 20:26:49 +0200

    Make some more (random :D :() connections

--- src/top.vhd
+++ src/top.vhd
@@ -30,8 +30,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity top is
-    Port ( GPIO_DIP_SW1 : in  STD_LOGIC;
-           GPIO_LED_0 : out  STD_LOGIC);
+    Port (
+        GPIO_DIP_SW1 : in  std_logic;
+        GPIO_LED_0 : out  std_logic;
+        SATA1_RX_N : in std_logic;
+        SATA1_RX_P : in std_logic;
+        SATA1_TX_N : out std_logic;
+        SATA1_TX_P : out std_logic
+   );
 end top;
 
 architecture Behavioral of top is
@@ -75,8 +81,8 @@ architecture Behavioral of top is
         TILE0_RXENPCOMMAALIGN0_IN               : in   std_logic;
         TILE0_RXENPCOMMAALIGN1_IN               : in   std_logic;
         ------------------- Receive Ports - RX Data Path interface -----------------
-        TILE0_RXDATA0_OUT                       : out  std_logic_vector(7 downto 0);
-        TILE0_RXDATA1_OUT                       : out  std_logic_vector(7 downto 0);
+        TILE0_RXDATA0_OUT                       : out  std_logic_vector(15 downto 0);
+        TILE0_RXDATA1_OUT                       : out  std_logic_vector(15 downto 0);
         TILE0_RXRECCLK0_OUT                     : out  std_logic;
         TILE0_RXRECCLK1_OUT                     : out  std_logic;
         TILE0_RXUSRCLK0_IN                      : in   std_logic;
@@ -86,10 +92,11 @@ architecture Behavioral of top is
         ------- Receive Ports - RX Driver,OOB signalling,Coupling and Eq.,CDR ------
         TILE0_RXELECIDLE0_OUT                   : out  std_logic;
         TILE0_RXELECIDLE1_OUT                   : out  std_logic;
-        TILE0_RXEQMIX0_IN                       : in   std_logic_vector(1 downto 0);
-        TILE0_RXEQMIX1_IN                       : in   std_logic_vector(1 downto 0);
-        TILE0_RXEQPOLE0_IN                      : in   std_logic_vector(3 downto 0);
-        TILE0_RXEQPOLE1_IN                      : in   std_logic_vector(3 downto 0);
+        -- Turned off.
+        -- TILE0_RXEQMIX0_IN                       : in   std_logic_vector(1 downto 0);
+        -- TILE0_RXEQMIX1_IN                       : in   std_logic_vector(1 downto 0);
+        -- TILE0_RXEQPOLE0_IN                      : in   std_logic_vector(3 downto 0);
+        -- TILE0_RXEQPOLE1_IN                      : in   std_logic_vector(3 downto 0);
         TILE0_RXN0_IN                           : in   std_logic;
         TILE0_RXN1_IN                           : in   std_logic;
         TILE0_RXP0_IN                           : in   std_logic;
@@ -108,8 +115,8 @@ architecture Behavioral of top is
         TILE0_TXCHARISK0_IN                     : in   std_logic;
         TILE0_TXCHARISK1_IN                     : in   std_logic;
         ------------------ Transmit Ports - TX Data Path interface -----------------
-        TILE0_TXDATA0_IN                        : in   std_logic_vector(7 downto 0);
-        TILE0_TXDATA1_IN                        : in   std_logic_vector(7 downto 0);
+        TILE0_TXDATA0_IN                        : in   std_logic_vector(15 downto 0);
+        TILE0_TXDATA1_IN                        : in   std_logic_vector(15 downto 0);
         TILE0_TXOUTCLK0_OUT                     : out  std_logic;
         TILE0_TXOUTCLK1_OUT                     : out  std_logic;
         TILE0_TXUSRCLK0_IN                      : in   std_logic;
@@ -282,8 +289,12 @@ architecture Behavioral of top is
         );
     END COMPONENT;
 
-    signal comm_init_detect_s : std_logic := 0;
-    signal comm_wake_detect_s : std_logic := 0;
+    signal comm_init_detect_s : std_logic;
+    signal comm_wake_detect_s : std_logic;
+    signal rx_elec_idle_s : std_logic;
+    signal tx_elec_idle_s : std_logic;
+    signal tx_is_k_s : std_logic;
+    signal rx_is_k_s : std_logic;
 
 begin
     GPIO_LED_0 <= GPIO_DIP_SW1;
@@ -349,13 +360,13 @@ begin
         link_layer_ready => ,
         phy_ready => ,
         tx_dout => ,
-        tx_is_k => ,
+        tx_is_k => tx_is_k_s,
         tx_comm_reset => ,
         tx_comm_wake => ,
-        tx_elec_idle => ,
+        tx_elec_idle => tx_elec_idle_s,
         rx_din => ,
-        rx_is_k => ,
-        rx_elec_idle => ,
+        rx_is_k => rx_is_k_s,
+        rx_elec_idle => rx_elec_idle_s,
         rx_byte_is_aligned => ,
         comm_init_detect => comm_init_detect_s,
         comm_wake_detect => comm_wake_detect_s,
@@ -404,8 +415,8 @@ begin
      sata_platform_i : SATA_PLATFORM
     generic map
     (
-        WRAPPER_SIM_GTPRESET_SPEEDUP    =>      1,
-        WRAPPER_SIM_PLL_PERDIV2         =>      x"14d"
+        WRAPPER_SIM_GTPRESET_SPEEDUP => 1,
+        WRAPPER_SIM_PLL_PERDIV2 => x"14d"
     )
     port map
     (
@@ -414,98 +425,112 @@ begin
         --TILE0  (X0Y3)
 
         ----------------------- Receive Ports - 8b10b Decoder ----------------------
-        TILE0_RXCHARISCOMMA0_OUT        =>      ,
-        TILE0_RXCHARISK0_OUT            =>      ,
-        TILE0_RXDISPERR0_OUT            =>      ,
-        TILE0_RXNOTINTABLE0_OUT         =>      ,
+        -- TODO: Is this really not connected to the SATA stack; is
+        -- TILE0_RXSTATUS0_OUT(2) sufficient?
+        TILE0_RXCHARISCOMMA0_OUT => open,
+        TILE0_RXCHARISK0_OUT => rx_is_k_s,
+        -- This should be connected to error phy_error
+        TILE0_RXDISPERR0_OUT =>      ,
+        -- This should be connected to error phy_error
+        -- This is signal indicating that invalid symbol (8B/10B) was received.
+        TILE0_RXNOTINTABLE0_OUT =>      ,
         ------------------- Receive Ports - Clock Correction Ports -----------------
-        TILE0_RXCLKCORCNT0_OUT          =>      ,
+        -- TODO: I'm completely lost with these signal...
+        TILE0_RXCLKCORCNT0_OUT =>      ,
         --------------- Receive Ports - Comma Detection and Alignment --------------
         -- ! This flags are set According to
         -- http://scholarworks.umass.edu/cgi/viewcontent.cgi?article=2250&context=theses
-        TILE0_RXENMCOMMAALIGN0_IN       =>      '1',
-        TILE0_RXENPCOMMAALIGN0_IN       =>      '1',
+        TILE0_RXENMCOMMAALIGN0_IN => '1',
+        TILE0_RXENPCOMMAALIGN0_IN => '1',
         ------------------- Receive Ports - RX Data Path interface -----------------
-        TILE0_RXDATA0_OUT               =>      ,
-        TILE0_RXRECCLK0_OUT             =>      ,
-        TILE0_RXUSRCLK0_IN              =>      ,
-        TILE0_RXUSRCLK20_IN             =>      ,
+        TILE0_RXDATA0_OUT => ,
+        TILE0_RXRECCLK0_OUT => ,
+        TILE0_RXUSRCLK0_IN => ,
+        TILE0_RXUSRCLK20_IN => ,
         ------- Receive Ports - RX Driver,OOB signalling,Coupling and Eq.,CDR ------
-        TILE0_RXELECIDLE0_OUT           =>      ,
-        TILE0_RXEQMIX0_IN               =>      ,
-        TILE0_RXEQPOLE0_IN              =>      ,
-        TILE0_RXN0_IN                   =>      ,
-        TILE0_RXP0_IN                   =>      ,
+        TILE0_RXELECIDLE0_OUT => rx_elec_idle_s,
+        -- Equalization setup
+        -- TODO: I have no idea what to do with this... Ok, it looks like I
+        -- have turned it off :D.
+        -- TILE0_RXEQMIX0_IN => ,
+        -- TILE0_RXEQPOLE0_IN => ,
+
+        -- Connect directly to the output pin.
+        -- SATA on the board is indexed from 1 not from zero so we use SATA 1.
+        TILE0_RXN0_IN => SATA1_RX_N,
+        TILE0_RXP0_IN => SATA1_RX_P,
         -------- Receive Ports - RX Elastic Buffer and Phase Alignment Ports -------
         -- TODO: is this correct? TILE0_RXSTATUS0_OUT(0) is TXCOMSTART and it
         -- is weird not to be connected...
-        TILE0_RXSTATUS0_OUT(0)          => open,
-        TILE0_RXSTATUS0_OUT(1)          => comm_wake_detect_s,
-        TILE0_RXSTATUS0_OUT(2)          => comm_init_detect_s,
+        TILE0_RXSTATUS0_OUT(0) => open,
+        TILE0_RXSTATUS0_OUT(1) => comm_wake_detect_s,
+        TILE0_RXSTATUS0_OUT(2) => comm_init_detect_s,
         ---------------- Transmit Ports - 8b10b Encoder Control Ports --------------
-        TILE0_TXCHARISK0_IN             =>      ,
+        TILE0_TXCHARISK0_IN => tx_is_k_s,
         ------------------ Transmit Ports - TX Data Path interface -----------------
-        TILE0_TXDATA0_IN                =>      ,
-        TILE0_TXOUTCLK0_OUT             =>      ,
-        TILE0_TXUSRCLK0_IN              =>      ,
-        TILE0_TXUSRCLK20_IN             =>      ,
+        TILE0_TXDATA0_IN => ,
+        TILE0_TXOUTCLK0_OUT =>      ,
+        TILE0_TXUSRCLK0_IN =>      ,
+        TILE0_TXUSRCLK20_IN =>      ,
         --------------- Transmit Ports - TX Driver and OOB signalling --------------
-        TILE0_TXN0_OUT                  =>      ,
-        TILE0_TXP0_OUT                  =>      ,
-        ----------------- Transmit Ports - TX Ports for PCI Express ----------------
-        TILE0_TXELECIDLE0_IN            =>      ,
+        -- Connect directly to the output pin.
+        -- SATA on the board is indexed from 1 not from zero so we use SATA 1.
+        TILE0_TXN0_OUT => SATA1_TX_N,
+        TILE0_TXP0_OUT => SATA1_TX_P,
+        TILE0_TXELECIDLE0_IN => tx_elec_idle_s,
         --------------------- Transmit Ports - TX Ports for SATA -------------------
-        TILE0_TXCOMSTART0_IN            =>      ,
-        TILE0_TXCOMTYPE0_IN             =>      ,
+        TILE0_TXCOMSTART0_IN =>      ,
+        TILE0_TXCOMTYPE0_IN =>      ,
         --------------------- Shared Ports - Tile and PLL Ports --------------------
-        TILE0_CLKIN_IN                  =>      ,
-        TILE0_GTPRESET_IN               =>      ,
-        TILE0_PLLLKDET_OUT              =>      ,
-        -- TODO: Why thus this exist?
-        TILE0_REFCLKOUT_OUT             =>      ,
-        TILE0_RESETDONE0_OUT            =>      ,
-        TILE0_RESETDONE1_OUT            =>      open,
+        TILE0_CLKIN_IN =>      ,
+        TILE0_GTPRESET_IN =>      ,
+        TILE0_PLLLKDET_OUT =>      ,
+        -- TODO: Why does this exist?
+        TILE0_REFCLKOUT_OUT =>      ,
+        TILE0_RESETDONE0_OUT =>      ,
+        TILE0_RESETDONE1_OUT => open,
 
 
 
         ----------------------- Receive Ports - 8b10b Decoder ----------------------
-        TILE0_RXCHARISK1_OUT            =>      open,
-        TILE0_RXDISPERR1_OUT            =>      open,
-        TILE0_RXCHARISCOMMA1_OUT        =>      open,
-        TILE0_RXNOTINTABLE1_OUT         =>      open,
+        TILE0_RXCHARISK1_OUT => open,
+        TILE0_RXDISPERR1_OUT => open,
+        TILE0_RXCHARISCOMMA1_OUT => open,
+        TILE0_RXNOTINTABLE1_OUT => open,
         ------------------- Receive Ports - Clock Correction Ports -----------------
-        TILE0_RXCLKCORCNT1_OUT          =>      open,
+        TILE0_RXCLKCORCNT1_OUT => open,
         --------------- Receive Ports - Comma Detection and Alignment --------------
-        TILE0_RXENMCOMMAALIGN1_IN       =>      '1',
-        TILE0_RXENPCOMMAALIGN1_IN       =>      '1',
+        TILE0_RXENMCOMMAALIGN1_IN => '1',
+        TILE0_RXENPCOMMAALIGN1_IN => '1',
         ------------------- Receive Ports - RX Data Path interface -----------------
-        TILE0_RXDATA1_OUT               =>      open,
-        TILE0_RXRECCLK1_OUT             =>      open,
-        TILE0_RXUSRCLK1_IN              =>      open,
-        TILE0_RXUSRCLK21_IN             =>      open,
+        TILE0_RXDATA1_OUT => open,
+        TILE0_RXRECCLK1_OUT => open,
+        TILE0_RXUSRCLK1_IN => open,
+        TILE0_RXUSRCLK21_IN => open,
         ------- Receive Ports - RX Driver,OOB signalling,Coupling and Eq.,CDR ------
-        TILE0_RXELECIDLE1_OUT           =>      open,
-        TILE0_RXEQMIX1_IN               =>      open,
-        TILE0_RXEQPOLE1_IN              =>      open,
-        TILE0_RXN1_IN                   =>      open,
-        TILE0_RXP1_IN                   =>      open,
+        TILE0_RXELECIDLE1_OUT => open,
+        -- Turned off.
+        -- TILE0_RXEQMIX1_IN => open,
+        -- TILE0_RXEQPOLE1_IN => open,
+        TILE0_RXN1_IN => open,
+        TILE0_RXP1_IN => open,
         -------- Receive Ports - RX Elastic Buffer and Phase Alignment Ports -------
-        TILE0_RXSTATUS1_OUT             =>      open,
+        TILE0_RXSTATUS1_OUT => open,
         ---------------- Transmit Ports - 8b10b Encoder Control Ports --------------
-        TILE0_TXCHARISK1_IN             =>      open,
+        TILE0_TXCHARISK1_IN => open,
         ------------------ Transmit Ports - TX Data Path interface -----------------
-        TILE0_TXDATA1_IN                =>      open,
-        TILE0_TXOUTCLK1_OUT             =>      open,
-        TILE0_TXUSRCLK1_IN              =>      open,
-        TILE0_TXUSRCLK21_IN             =>      open,
+        TILE0_TXDATA1_IN => open,
+        TILE0_TXOUTCLK1_OUT => open,
+        TILE0_TXUSRCLK1_IN => open,
+        TILE0_TXUSRCLK21_IN => open,
         --------------- Transmit Ports - TX Driver and OOB signalling --------------
-        TILE0_TXN1_OUT                  =>      open,
-        TILE0_TXP1_OUT                  =>      open,
+        TILE0_TXN1_OUT => open,
+        TILE0_TXP1_OUT => open,
         ----------------- Transmit Ports - TX Ports for PCI Express ----------------
-        TILE0_TXELECIDLE1_IN            =>      open,
+        TILE0_TXELECIDLE1_IN => open,
         --------------------- Transmit Ports - TX Ports for SATA -------------------
-        TILE0_TXCOMSTART1_IN            =>      open,
-        TILE0_TXCOMTYPE1_IN             =>      open
+        TILE0_TXCOMSTART1_IN => open,
+        TILE0_TXCOMTYPE1_IN => open
     );
 
 
@@ -516,9 +541,9 @@ begin
     tile0_refclk_ibufds_i : IBUFDS
     port map
     (
-        O                               =>      ,
-        I                               =>      ,  -- Connect to package pin Y4
-        IB                              =>        -- Connect to package pin Y3
+        O =>      ,
+        I =>      ,  -- Connect to package pin Y4
+        IB =>        -- Connect to package pin Y3
     );
 
 
